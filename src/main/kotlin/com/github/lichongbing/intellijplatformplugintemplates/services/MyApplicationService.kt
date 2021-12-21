23927585797d3210b86ed66cfc9e4c97cb7c776d
GitHub Action <action@github.com>
2021-12-21 15:30:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lichongbing/intellijplatformplugintemplates/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lichongbing.intellijplatformplugintemplates.services
+
+import com.github.lichongbing.intellijplatformplugintemplates.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/lichongbing/intellijplatformplugintemplates/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lichongbing/intellijplatformplugintemplates/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lichongbing.intellijplatformplugintemplates.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lichongbing.intellijplatformplugintemplates.MyBundle
 
 class MyProjectService(project: Project) {
 
